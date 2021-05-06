981512b85b13ca18e520670c9417c3fbc1e205d0
Rajesh Khatwa <rajesh.khatwa@gmail.com>
2021-05-06 18:25:23 +0530

    iouou

--- student/src/main/java/com/rk/bulletin/HelloController.java
+++ student/src/main/java/com/rk/bulletin/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String sayHello()
 	{
- return "I am at port 8090 check it 2";
+ return "I am at port 8090 check it 26";
 }
 	
 	
